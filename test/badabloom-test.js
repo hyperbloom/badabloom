1d0466d2d942189d975f4647f398c6415cd3b5c0
Fedor Indutny <fedor@indutny.com>
2017-04-21 18:06:29 -0400

    lib: put key+value into the filter

--- test/badabloom-test.js
+++ test/badabloom-test.js
@@ -7,32 +7,36 @@ const BadaBloom = require('../');
 describe('BadaBloom', () => {
   let b;
 
+  function B(str) {
+    return Buffer.from(str);
+  }
+
   beforeEach(() => {
     b = new BadaBloom();
   });
 
   it('should `.insert()` and `.fetch()` should return value', () => {
-    b.insert('hello', 'world');
+    b.insert(B('hello'), B('world'));
 
-    const r = b.fetch('hello');
-    assert.deepEqual(r, [ 'world' ]);
+    const r = b.fetch(B('hello'));
+    assert.deepEqual(r, [ B('world') ]);
   });
 
   it('should `.bulkInsert()`', () => {
-    b.bulkInsert([ { key: 'hello', value: 'world' },
-                   { key: 'ohai', value: 'everyone' } ]);
+    b.bulkInsert([ { key: B('hello'), value: B('world') },
+                   { key: B('ohai'), value: B('everyone') } ]);
 
-    const r = b.fetch('hello');
-    assert.deepEqual(r, [ 'world' ]);
+    const r = b.fetch(B('hello'));
+    assert.deepEqual(r, [ B('world') ]);
   });
 
   it('should `.query()` missing items', () => {
     const other = new BadaBloom();
 
     for (let i = 0; i < 750; i++)
-      b.insert(i.toString(), i);
+      b.insert(B((i >>> 1).toString()), B(i.toString()));
     for (let i = 250; i < 1000; i++)
-      other.insert(i.toString(), i);
+      other.insert(B((i >>> 1).toString()), B(i.toString()));
 
     const self = b.query(b.getRawFilter());
     assert.equal(self.length, 0);
@@ -42,21 +46,21 @@ describe('BadaBloom', () => {
   });
 
   it('should `.fetch()` duplicates', () => {
-    b.insert('hello', 'world');
-    b.insert('hello', 'everyone');
-    b.insert('ohai', 'friends');
+    b.insert(B('hello'), B('world'));
+    b.insert(B('hello'), B('everyone'));
+    b.insert(B('ohai'), B('friends'));
 
-    const results = b.fetch('hello');
-    assert.deepEqual(results, [ 'world', 'everyone' ]);
+    const results = b.fetch(B('hello'));
+    assert.deepEqual(results, [ B('world'), B('everyone') ]);
   });
 
   it('should `.has()` duplicates', () => {
-    b.insert('hello', 'world');
-    b.insert('hello', 'everyone');
+    b.insert(B('hello'), B('world'));
+    b.insert(B('hello'), B('everyone'));
 
-    assert(b.has('hello', 'world'));
-    assert(b.has('hello', 'everyone'));
-    assert(!b.has('hello', 'someone'));
-    assert(!b.has('ohai', 'someone'));
+    assert(b.has(B('hello'), B('world')));
+    assert(b.has(B('hello'), B('everyone')));
+    assert(!b.has(B('hello'), B('someone')));
+    assert(!b.has(B('ohai'), B('someone')));
   });
 });
